4b28372639e4e97096ab636a7c9b7d070512ebc7
Tatyana McGuire <tatyanam953@gmail.com>
2019-02-04 23:14:39 +0000

    my changes

--- js/script.js
+++ js/script.js
@@ -1,10 +1,29 @@
 /* global $ */
 
 // BELOW Update the songs array with four of your favorites songs.
-var songs = [];
+var songs = ["voice in my head", 
+             "shot clock",
+             "thank you next",
+             "come through and chill"];
 // BELOW Add 4 More arrays to store images_links, artists, song lengths, and links for each song
 // Make sure they match the same order as your array above
+var store images_urls = ["https://images.genius.com/557f4ca436aaabc4210bd3850f96b954.1000x1000x1.jpg",
+"https://i.ytimg.com/vi/XgH93HlqtQE/maxresdefault.jpg",
+"https://upload.wikimedia.org/wikipedia/en/thumb/b/b2/Ariana_Grande_Thank_U_Next.png/220px-Ariana_Grande_Thank_U_Next.png",
+"https://images-na.ssl-images-amazon.com/images/I/61hK6c6xl9L._SS500.jpg"]
 
+
+
+var artists = ["Aboogie","Ella Mai", "Ariana Grande","Miguel ft J. Cole"]
+
+
+
+var song length =["2:23",]
+
+
+
+
+var links =[]
 
 
 function displaySongInfo(){
